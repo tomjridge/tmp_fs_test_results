be0d13ab516c6c8fba09e8954488e735f698dcc6
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2014-01-14 09:55:24 +0000

    +

---
+++ 2013-10-23_posix_adhoc_rename_2/test.sh
@@ -0,0 +1,6 @@
+export OCAMLRUNPARAM="s=8M,l=8M,i=1M"
+rm -rf /tmp/tmp2
+mkdir /tmp/tmp2
+/tmp/l/general/research/fs/fs_test/posix/posix -b -r /tmp/tmp2 /tmp/l/general/research/fs/fs_test/adhoc_tests/adhoc_rename_tests >adhoc_rename_tests_posix_test_results
+
+
