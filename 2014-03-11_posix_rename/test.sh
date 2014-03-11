476139a3e179813db12a580bfdb370d1ee71ae82
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2014-03-11 14:15:52 +0000

    +

---
+++ 2014-03-11_posix_rename/test.sh
@@ -0,0 +1,6 @@
+export OCAMLRUNPARAM="s=8M,l=8M,i=1M"
+rm -rf /tmp/tmp2
+mkdir /tmp/tmp2
+touch tests.started
+/tmp/l/bitbucket/fs/fs_test/posix/posix -b true -r /tmp/tmp2 tests >posix_test_results
+
