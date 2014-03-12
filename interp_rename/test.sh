8dc9a50a84366d60119350457c0e3ed742ca449a
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2014-03-12 09:50:28 +0000

    +

--- interp_rename/test.sh
+++ interp_rename/test.sh
@@ -2,4 +2,4 @@ export OCAMLRUNPARAM="s=8M,l=8M,i=1M"
 cp ../current_tests/rename.tests ./tests
 touch started
 echo "started"
-/tmp/l/bitbucket/fs/fs_test/interp.native -b -m dir_heap tests > without_results
+/tmp/l/bitbucket/fs/fs_test/interp.native -b -m dir_heap tests > interp_test_results
--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date=`date +'%F'`
-
+testdiff=/tmp/l/bitbucket/fs/fs_test/testdiff.native
 
 TMP2=`mktemp -d --tmpdir=. _${date}_posix_rename_XXX`
 echo Running posix_rename tests in $TMP2
@@ -13,4 +13,7 @@ echo Running interp_rename tests in $TMP1
 cp -R interp_rename/* $TMP1
 (cd $TMP1 && ./test.sh)
 
-
+echo Diffing...
+cmd="$testdiff $TMP2/posix_test_results $TMP1/interp_test_results"
+echo $cmd > _rename.diff
+$cmd >>_rename.diff
