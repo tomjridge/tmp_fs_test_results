c9e9b293e23dabd22066bf9591206eeefe90233f
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2014-04-03 11:59:32 +0100

    First run of open tests. Lots of Linux specific behaviour, of course.

---
+++ 2014-04-03_interp_open_8C1/test.sh
@@ -0,0 +1,6 @@
+# first arg is name of test file
+export OCAMLRUNPARAM="s=8M,l=8M,i=1M"
+cp $1 ./tests
+touch started
+echo "started"
+../src_ext/fs/fs_test/interp.native -b true -m dir_heap tests > interp_test_results
---
+++ 2014-04-03_posix_open_vNP/test.sh
@@ -0,0 +1,8 @@
+# first argument is name of test file
+export OCAMLRUNPARAM="s=8M,l=8M,i=1M"
+cp $1 ./tests
+rm -rf /tmp/tmp2
+mkdir /tmp/tmp2
+touch tests.started
+../src_ext/fs/fs_test/posix/posix -b true -r /tmp/tmp2 tests >posix_test_results
+
