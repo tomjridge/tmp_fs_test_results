2c66a20bc389006f282b44b97468e25756e8b1be
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2014-05-12 15:32:34 +0100

    stat and close

--- 2014-05-12_posix_stat_Tcj/test.sh
+++ 2014-05-12_posix_stat_Tcj/test.sh
@@ -4,7 +4,5 @@ cp $1 ./tests
 rm -rf /tmp/tmp2
 mkdir /tmp/tmp2
 touch tests.started
-# make sure fd 3 is closed - somehow this fd 3 is opened by some other program
-exec 3>&-
 ../src_ext/fs/fs_test/posix/posix -b true -r /tmp/tmp2 tests >posix_test_results
 
