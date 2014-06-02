554a59381da5b15969356b4b00102c99abaf11ca
Thomas Tuerk <thomas@tuerk-brechen.de>
2014-06-02 09:49:01 +0100

    add option -dummy to run_tests
    the script "run_tests" now allows for an extra option "-dummy". If this
    is given, a test-directory with prefix "dummy_" is created. The
    intention is that these dummy-directories can easily be deleted
    afterwards and are used for local, very temporary testing. Real tests
    should be run without this option. Then the old naming sheme applies and
    the results are automatically staged for git (but not commited
    automatically).

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,14 @@
 # call this script with the argument directory of traces to test test 
 # e.g. ./this_script.sh current_tests/open_read_write
 
+if [[ $1 == "-git" ]]
+then
+    shift;
+    real=1;
+else
+    real=0;
+fi
+
 test_dir=$1
 if [[ ! (-d $test_dir) ]]
 then
@@ -11,9 +19,16 @@ then
 fi
 
 date=`date +'%F'`
-
 test_dir_name=`basename $test_dir`
-OUTDIR=`mktemp -d --tmpdir=. ${date}_${test_dir_name}_XXX`
+
+if [[ $real -eq 1 ]] 
+then
+    OUTDIR=`mktemp -d --tmpdir=. ${date}_${test_dir_name}_XXX`
+else
+    OUTDIR=`mktemp -d --tmpdir=. dummy_${test_dir_name}_XXX`
+fi
+
+echo "creating test-directory $OUTDIR ..."
 cp $test_dir/* $OUTDIR
 mkdir $OUTDIR/results
 
@@ -27,6 +42,13 @@ for file in *.trace; do
 done
 
 echo -e "\ndone!"
+
+if [[ $real -eq 1 ]]
+then
+    cd ..
+    echo "staging $OUTDIR ..."
+    git stage $OUTDIR
+fi
 
 exit 0
 
